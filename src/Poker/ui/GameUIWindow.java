0b5011e950567c25ed7cc1cb60e891bcfe72440c
WillieXia <xiaw@rpi.edu.com>
2020-08-06 21:50:50 -0400

    init ui

--- src/Poker/ui/GameUIWindow.java
+++ src/Poker/ui/GameUIWindow.java
@@ -1,12 +1,22 @@
 package Poker.ui;
 
+import Poker.components.*;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.event.*;
 import java.awt.*;
 
 
-public class GameUIWindow
+public class GameUIWindow extends JFrame
 {
+    public GameUIWindow()
+    {
+        initUI();
+    }
 
+    private void initUI()
+    {
+
+    }
 }
