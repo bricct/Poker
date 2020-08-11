37a7896e0bea2e0a863224e91ab0dc4f4823a876
WillieXia <xiaw@rpi.edu.com>
2020-08-11 18:00:10 -0400

    game loop

--- src/Poker/game/Game.java
+++ src/Poker/game/Game.java
@@ -21,10 +21,70 @@ public class Game()
         }
     }
 
+    private class PlayerTuple()
+    {
+        Player player;
+        int currMoney;
+        public PlayerTuple(Card iplayer, int icurrMoney)
+        {
+            player = iplayer;
+            currMoney = icurrMoney;
+        }
+    }
+
+    public int goAroundTable(ArrayList<CardTuple> queue, int pot)
+    {
+        boolean check = false;
+        int currentCheckPay = 0;
+        boolean initialChecked = false;
+        while(!check)
+        {
+            for(int i=0;i<queue.size();i++)
+            {
+                
+                //initialChecked is for checking if it's an initialBet. After initial bet, we break at the last better
+                if(initialChecked && queue.get(i).currMoney == currentCheckPay)
+                {
+                    return pot;
+                }else{//Do the action buttons: bet, check, fold
+                    if(true)//If player checks...
+                    {
+                        queue.get(i).player.subMoney(currentCheckPay - queue.get(i).currMoney);
+                        pot += currentCheckPay - queue.get(i).currMoney;
+                        queue.get(i).currMoney = currentChecKPay; //player set to current pot amount
+                    }else if(true) //IF bet
+                    {
+                        int bet = 0; //<--------- CHANGE THIS
+                        currentCheckPay += bet;
+                        queue.get(i).player.subMoney(currentCheckPay - queue.get(i).currMoney);
+                        pot += currentCheckPay - queue.get(i).currMoney;
+                        queue.get(i).currMoney = currentChecKPay;
+                    }else //Fold
+                    {
+                        queue.remove(i);
+                        if(queue.size() == 1)
+                        {
+                            return pot;
+                        }
+                    }
+                }
+            }
+            iniialChecked = true;
+            check = true;
+            for(int i=0;i<queue.size();i++)
+            {
+                if(queue.get(i).currMoney != currentCheckPay);
+                {
+                    check = false;
+                }
+            }
+        }
+    }
+
     public void playRound()
     {
         int pot = 0;
-        ArrayList<Player> queue = new ArrayList<Player>();
+        ArrayList<CardTuple> queue = new ArrayList<CardTuple>();
         boolean smallBet = false;
         boolean bigBet = false;
         //Add playable players to queue
@@ -32,7 +92,7 @@ public class Game()
         {
             if(players.get(j).getMoney() > 0)
             {
-                queue.add(players.get(j));
+                queue.add(new CardTuple(players.get(j),0));
             }
         }
         if(queue.size() == 1)
@@ -40,53 +100,80 @@ public class Game()
             return; //1 player left
         }
 
-        players.get(0).subMoney(small); //Big Blind    
-        players.get(1).subMoney(big); //Small blind          
+        queue.get(0).player.subMoney(small); //Big Blind    
+        queue.get(1).player.subMoney(big); //Small blind          
 
         //Deal cards to players in queue
         for(int i=0;i<2;i++)
         {
             for(int j=0;j<queue.size();j++)
             {
-                queue.get(j).addToHand(deck.draw());
+                queue.get(j).player.addToHand(deck.draw());
             }
         }
 
-        boolean check = false;
-        int checkedPlayers = 0;
-        while(!check)
+        //pre-flop
+        queue.get(0).currMoney = -1 * small; //small still has to pay
+        pot = goAroundTable(queue, pot); //updated pot, queue is passed by reference
+        if(queue.size() == 0)
         {
-            for(int i=0;i<queue.size();i++)
-            {
-                //Do the action buttons: bet, check, fold
-                //If player checks...
-                if(true)
-                {
-                    checkedPlayers++;
-                }else if(true) //IF bet
-                {
-
-                }else //Fold
-                {
-                    queue.remove(queue.get(i));
-                }
-            }
-            if(queue.size() == checkedPlayers)
-            {
-                check = true;
-            }
+            queue.get(0).player.addMoney(pot);
         }
-
+        //flop
         for(int i=0;i<3;i++)
         {
             boardCards.add(deck.draw());
         }
+        pot = goAroundTable(queue, pot); //updated pot, queue is passed by reference
+        if(queue.size() == 0)
+        {
+            queue.get(0).player.addMoney(pot);
+        }
+        //Turn
+        boardCards.add(deck.draw());
+        pot = goAroundTable(queue, pot); //updated pot, queue is passed by reference
+        if(queue.size() == 0)
+        {
+            queue.get(0).player.addMoney(pot);
+        }
+        //River
+        boardCards.add(deck.draw());
+        pot = goAroundTable(queue, pot); //updated pot, queue is passed by reference
+        if(queue.size() == 0)
+        {
+            queue.get(0).player.addMoney(pot);
+        }else
+        {
+            ArrayList<Combo> combos = new ArrayList<Combo>();
+            for(int i=0;i<queue.size();i++)
+            {
+                ArrayList<Card> playerhand = new ArrayList<Card>();
+                playerhand.add(queue.get(i).firstCard()).add(queue.get(i).secondCard());
+                combos.add(new Combo(playerhand, boardCards));
+            }
+            //You can use this for displaying in the UI
+            Combo bestCombo = combos.get(0);
+            int itr = 0;
+            for(int i=0;i<queue.size();i++)
+            {
+                if(combos.get(i) > bestCombo)
+                {
+                    itr = i;
+                    bestCombo = combos.get(i);
+                }
+            }
+            //WINNER! //you can get hand cards straight from player
+            queue.get(itr).player.addMoney(pot);
+        }
 
-        
 
-
-        //Remove first element, add to end
+        //Remove first element, add to end (Rotation of players)
         players.add(players.remove(0));
+        for(Player player : players)
+        {
+            player.clearHand();
+        }
+        boardCards.clear();
         deck.reset();
     }
 }
