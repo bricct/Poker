788cbe9ee47efaae5459f6d80d1df7e4aa3a49ed
Trey Briccetti <bricct@rpi.edu>
2020-08-21 06:34:55 -0400

    scripts

--- scripts/Windows/Compiler.cmd
+++ scripts/Windows/Compiler.cmd
@@ -1,10 +1,10 @@
 @echo off
 cd ..
 cd..
-javac -cp "bin" -d "bin" ./src/game/*.java
-javac -cp "bin" -d "bin" ./src/netwrk/*.java
-javac -cp "bin" -d "bin" ./src/sound/*.java
-javac -cp "bin" -d "bin" ./src/ui/*.java
-javac -cp "bin" -d "bin" ./src/ui/frames/*.java
-javac -cp "bin" -d "bin" ./src/ui/panels/*.java
+javac -cp "bin" -d "bin" -sourcepath "src" ./src/game/*.java
+javac -cp "bin" -d "bin" -sourcepath "src" ./src/netwrk/*.java
+javac -cp "bin" -d "bin" -sourcepath "src" ./src/sound/*.java
+javac -cp "bin" -d "bin" -sourcepath "src" ./src/ui/*.java
+javac -cp "bin" -d "bin" -sourcepath "src" ./src/ui/frames/*.java
+javac -cp "bin" -d "bin" -sourcepath "src" ./src/ui/panels/*.java
 pause
